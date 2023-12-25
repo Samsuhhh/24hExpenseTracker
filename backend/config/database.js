0246d054540311405556f664010fd17ad033b581
Samuel Suh <samsuh@Samuels-MacBook-Pro-2.local>
2023-12-24 17:25:34 -0800

    udpate database.js file

--- backend/config/database.js
+++ backend/config/database.js
@@ -11,15 +11,15 @@ module.exports = {
     logQueryParameters: true,
     typeValidation: true
   },
-//   production: {
-//     use_env_variable: 'DATABASE_URL',
-//     dialect: 'postgres',
-//     seederStorage: 'sequelize',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     }
-//   }
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
